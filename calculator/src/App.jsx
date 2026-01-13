91d9647539f7cc009377b20dcdd77cfc6abc12be
kanchan kahar <kaharkanchan884@gmail.com>
2026-01-13 16:48:27 +0530

    added new feature

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Calculator from './Calculator'
-import Addition from './Addition'
-import Sub from './Sub'
-import Multiplication from './Multiplication'
-import Division from './Division'
+
 
 
 const App = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Calculator />} />
-        <Route path='/add' element={<Addition />} />
-        <Route path='/sub' element={<Sub />} />
-        <Route path='/multiply' element={<Multiplication />} />
-        <Route path='/divide' element={<Division />} />
-      </Routes>
+      <Calculator/>
     </>
   )
 }
